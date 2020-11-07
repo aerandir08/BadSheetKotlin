fde5fa613babee2b48bb959af80bf8d9a64462ec
Malte Becker <malte91@web.de>
2020-11-07 14:22:38 +0100

    add first activities with layout first activity switches

--- app/src/main/java/de/malte/badsheet/MainActivity.kt
+++ app/src/main/java/de/malte/badsheet/MainActivity.kt
@@ -1,11 +1,34 @@
 package de.malte.badsheet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when Button Match Settings is clicked. **/
+    fun open_match_settings(view: View)
+    {
+        val intent = Intent(this, MatchSettings::class.java).apply {}
+        startActivity(intent)
+    }
+
+    /** Called when a Team Button is clicked. **/
+    fun open_team(view: View)
+    {
+        val intent = Intent(this, Team::class.java).apply {/*TODO: Hier muss das Klassenobjekt des jeweiligen Teams rein.*/}
+        startActivity(intent)
+    }
+
+    /** Called when Button Scoreboard is clicked. **/
+    fun open_scoreboard(view: View)
+    {
+        val intent = Intent(this, Scoreboard::class.java).apply {}
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/de/malte/badsheet/MatchSettings.kt
@@ -0,0 +1,11 @@
+package de.malte.badsheet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchSettings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_settings)
+    }
+}
