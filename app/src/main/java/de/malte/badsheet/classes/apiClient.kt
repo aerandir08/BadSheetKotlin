d0e0547092eefbe261da850cc8998cec1d80f568
Malte Becker <malte.becker91@gmail.com>
2022-02-25 20:20:33 +0100

    At check if server is available

--- app/src/main/java/de/malte/badsheet/classes/apiClient.kt
+++ app/src/main/java/de/malte/badsheet/classes/apiClient.kt
@@ -1,34 +1,62 @@
 package de.malte.badsheet.classes
 
 import io.swagger.client.apis.DefaultApi
+import java.net.InetAddress
 
 class apiClient()
 {
     private val client = DefaultApi()
     fun get_teamnames(): MutableList<String>
     {
-        val teams = client.teamsGet()
-        val returnList : MutableList<String> = arrayListOf()
-        for(team in teams)
+        if (isInternetAvailable())
         {
-            returnList.add(team.name)
+            val teams = client.teamsGet()
+            val returnList: MutableList<String> = arrayListOf()
+            for (team in teams) {
+                returnList.add(team.name)
+            }
+            return returnList
         }
-        return returnList
+        else
+        {
+            return arrayListOf()
+        }
     }
 
     fun update_teamnames()
     {
-        client.teamsPost()
+        if (isInternetAvailable())
+        {
+            client.teamsPost()
+        }
     }
 
     fun get_players(teamname: String): MutableList<String>
     {
-        val players = client.playersTeamnameGet(teamname)
-        val returnList : MutableList<String> = arrayListOf()
-        for(player in players)
+        if (isInternetAvailable())
         {
-            returnList.add(player.name)
+            val players = client.playersTeamnameGet(teamname)
+            val returnList: MutableList<String> = arrayListOf()
+            for (player in players) {
+                returnList.add(player.name)
+            }
+            return returnList
         }
-        return returnList
+        else
+        {
+            return arrayListOf()
+        }
+    }
+
+    private fun isInternetAvailable(): Boolean
+    {
+        try {
+            val ipAddr: InetAddress = InetAddress.getByName("45.94.211.170")
+             return!ipAddr.equals("")
+        }
+        catch (e: Exception)
+        {
+            return false
+        }
     }
 }
